5bac6770e192a68a3d2325ac1c17e057fc521565
saurabh daswant <77434364+Saurabhdaswant@users.noreply.github.com>
2021-03-08 19:30:26 +0530

    changes

--- main.js
+++ main.js
@@ -10,20 +10,20 @@ window.addEventListener("DOMContentLoaded", function () {
   
     var form = document.getElementById("my-form");
     // var button = document.getElementById("my-form-button");
-    var status = document.getElementById("status");
+    // var status = document.getElementById("status");
   
     // Success and Error functions for after the form is submitted
   
-    function success() {
-      form.reset();
-      status.classList.add("success");
-      status.innerHTML = "Thanks!";
-    }
+    // function success() {
+    //   form.reset();
+    //   status.classList.add("success");
+    //   status.innerHTML = "Thanks!";
+    // }
   
-    function error() {
-      status.classList.add("error");
-      status.innerHTML = "Oops! There was a problem.";
-    }
+    // function error() {
+    //   status.classList.add("error");
+    //   status.innerHTML = "Oops! There was a problem.";
+    // }
   
     // handle the form submission event
   
