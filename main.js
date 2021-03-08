4816e56f1b00f0cdc7ad4ddb8e41823901f9e9e7
saurabh daswant <77434364+Saurabhdaswant@users.noreply.github.com>
2021-03-08 19:48:18 +0530

    update main.js

--- main.js
+++ main.js
@@ -1,29 +1,22 @@
-function onClickMenu(){
-    
-    document.getElementById("menu").classList.toggle("change");
-    document.getElementById("nav").classList.toggle("change");
-}
-
-
 window.addEventListener("DOMContentLoaded", function () {
     // get the form elements defined in your form HTML above
   
     var form = document.getElementById("my-form");
     // var button = document.getElementById("my-form-button");
-    // var status = document.getElementById("status");
+    var status = document.getElementById("status");
   
     // Success and Error functions for after the form is submitted
   
-    // function success() {
-    //   form.reset();
-    //   status.classList.add("success");
-    //   status.innerHTML = "Thanks!";
-    // }
+    function success() {
+      form.reset();
+      status.classList.add("success");
+      status.innerHTML = "Thanks!";
+    }
   
-    // function error() {
-    //   status.classList.add("error");
-    //   status.innerHTML = "Oops! There was a problem.";
-    // }
+    function error() {
+      status.classList.add("error");
+      status.innerHTML = "Oops! There was a problem.";
+    }
   
     // handle the form submission event
   
